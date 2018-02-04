13fbc1db8b7d3e7be47997436229297623e3782f
JooChan <14jshin@gmail.com>
2018-02-03 21:43:25 -0500

    Added methods

--- complex.py
+++ complex.py
@@ -1,18 +1,48 @@
 from math import *
-
+import cmath
 
 class Complex:
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.real = x
+        self.imag = y
 
-    def get_x(self):
-        return self.x
+    def getReal(self):
+        return self.real
 
-    def get_y(self):
-        return self.y
+    def getImaginary(self):
+        return self.imag
 
+    def __add__(self, other):
+        x = self.x
+        y = self.y
+        a = other.getReal()
+        b = other.getImaginary()
+        return (a - x) + ((b - y) * 1j)
+
+    def __sub__(self, other):
+        x = self.x
+        y = self.y
+        a = other.getReal()
+        b = other.getImaginary()
+        return (a - x) + ((b - y) * 1j)
+
+    def __mult__(self, other):
+        x = self.x
+        y = self.y
+        a = other.getReal()
+        b = other.getImaginary()
+        return ((a * x) - (b * y)) + (((a * y) + (b * x)) * 1j)
+
+    def __abs__(self):
+        x = self.x
+        y = self.y
+        return (((x ** 2) + (y ** 2)) ** .5)
+
+    def __str__(self):
+        return 'Real: %s \nImag: %s' % (self.real, self.imag)
 
 z = Complex(5, 5)
-print Complex.get_x(z)
-print Complex.get_y(z)
+
+print z.getReal()
+print z.__str__()
+
