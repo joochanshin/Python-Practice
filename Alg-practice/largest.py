3fed71e882ea8fcfe575e946a3f27fcba2bd2a2a
JooChan <14jshin@gmail.com>
2018-02-16 12:47:16 -0500

    finished alg

--- Alg-practice/largest.py
+++ Alg-practice/largest.py
@@ -2,3 +2,14 @@ arr1 = ["A", "B", "C", "A", "B"]
 
 print(arr1)
 
+
+def first_recurring(arr):
+    counts = {}
+    for char in arr:
+        if char in counts:
+            return char
+        counts[char] = 1
+    return None
+
+
+print(first_recurring(arr1))
